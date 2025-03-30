f8ed6db0289bd1881e7386ed76bf426e9c35c6a7
Bhuvanshankar <125850365+Bhuvan411@users.noreply.github.com>
2025-03-30 09:00:17 +0530

    Add files via upload

---
+++ day1/chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		
+		byte age = 30;
+		System.out.println( age);
+		
+		short emergency = 108;
+		System.out.println(emergency);
+		
+		int salary = 25000;
+		System.out.println(salary);
+		
+		long mobile_number = 1234567L;
+		System.out.println(mobile_number);
+		
+		float usd = 0.12F;
+		System.out.println(usd);
+		
+
+	}
+
+}
